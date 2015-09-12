a9d59ebb3f1f3f29c422d60df2d06090328bf44c
Robert Bond III <drummerkid5694@hotmail.com>
2015-09-11 21:16:44 -0400

    Got rid of Framerate counter and added RedditSharp into project

--- RedditClient/RedditClient/MainPage.xaml.cs
+++ RedditClient/RedditClient/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RedditSharp;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,11 +21,11 @@ namespace RedditClient
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
